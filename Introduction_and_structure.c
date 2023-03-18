81447c8131bf530628b494eb8b6ebce93aec4561
Rustic Reboot <128249305+RusticReboot@users.noreply.github.com>
2023-03-18 23:19:30 +0530

    Add files via upload

---
+++ Introduction_and_structure.c
@@ -0,0 +1,17 @@
+#include <stdio.h> //#include is the preprocessor directive <stdio.h>-- header file
+
+int main()
+{ // main function declaration-- this is where all of the codes are written
+
+    // your code here
+
+    printf("hello world"); // printf prints whatever is written inside it
+
+    return 0; // return marks the end of main function
+
+    // statements after return are not executed
+
+    // ; marks the end of line and every line ends with a ;
+}
+
+// printf("hello world"); // statements outside main are not executed
